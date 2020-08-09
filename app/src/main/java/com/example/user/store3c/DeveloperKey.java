df1472332d48e76757ed7c4825df88a1a0eb3575
Alex <lyenh123@gmail.com>
2020-08-09 21:05:20 +0800

    Delete DeveloperKey.java

--- app/src/main/java/com/example/user/store3c/DeveloperKey.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.store3c;
-
-final class DeveloperKey {
-
-    private DeveloperKey() {
-    }
-
-    static final String YOUTUBE_API_KEY = "AIzaSyDdubjzJnXRydm2B7pmPjXfU4UUYF6iLO8";
-}
