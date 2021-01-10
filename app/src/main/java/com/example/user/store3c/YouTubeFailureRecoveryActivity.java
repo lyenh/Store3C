b7d69b8c603c2ca2a369137499f1957ed9ffac6b
Alex <lyenh123@gmail.com>
2021-01-11 02:02:46 +0800

    Add the file to fix the bug of playing reentry in another page running the new YouTube video.

---
+++ app/src/main/java/com/example/user/store3c/YouTubeFailureRecoveryActivity.java
@@ -0,0 +1,43 @@
+package com.example.user.store3c;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import static com.example.user.store3c.DeveloperKey.YOUTUBE_API_KEY;
+
+/**
+ * An abstract activity which deals with recovering from errors which may occur during API
+ * initialization, but can be corrected through user action.
+ */
+public abstract class YouTubeFailureRecoveryActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        Toast.makeText(YouTubeFailureRecoveryActivity.this, "YouTube initialize error", Toast.LENGTH_LONG).show();
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
+
+}
